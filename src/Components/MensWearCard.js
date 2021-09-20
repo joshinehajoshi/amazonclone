6d516d96727b77312a4712bb137067c6d77209e6
Neha Joshi <nehajoshigkp@gmail.com>
2021-09-20 09:19:44 +0530

    .

--- src/Components/MensWearCard.js
+++ src/Components/MensWearCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import '../css/ProductCard.css';
 import { useDispatch } from 'react-redux';
@@ -18,9 +18,14 @@ function MensWearCard(props) {
                <h5>Price: {props.price}</h5>
                 </Card.Text>
                 <Card.Text>
-                <h5>Details: <h6>
+                <h6>
                 Some quick example text to build on the card title and make up the bulk of
-                the card's content.</h6></h5>
+                the card's content.</h6>
+                </Card.Text>
+                <Card.Text className="quantity-wrapper">
+               <Button variant="dark" className="quantity-btn"><i class="fa fa-minus" aria-hidden="true"></i></Button>
+               <input className="quantity-input"/>
+               <Button variant="dark" className="quantity-btn"><i class="fa fa-plus" aria-hidden="true"></i></Button>
                 </Card.Text>
                 <Button variant="primary" className="add-to-cart" onClick={() => dispatch(counterAction(props))}>Add To Cart</Button>
             </Card.Body>
