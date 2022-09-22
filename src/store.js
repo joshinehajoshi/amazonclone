954191567c34f4c5f3eb0b42d6a5f83eb2571014
Neha Joshi <40416505+joshinehajoshi@users.noreply.github.com>
2022-09-22 21:59:18 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -5,5 +5,4 @@ import rootReducers from './reducers/index';
 const store = createStore(rootReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-
 export default store;
